47dbdc847c63d2805f91665f0cdc6c071b0c24a1
gugy <ggyrst@gmail.com>
2022-08-31 11:08:11 -0400

    getting data pandas_datareader + more

---
+++ functions.py
@@ -0,0 +1,44 @@
+from math import log, sqrt
+
+def display_as_percentage( value):   
+    return '{:.1f}%'.format(value * 100)
+
+def simple_return(start_price, end_price, dividend=0):
+    return ((end_price - start_price + dividend)/start_price)
+
+def log_return( start_price, end_price):
+    return log(end_price) - log(start_price)
+
+def annualize_return(log_return, t):
+    return log_return*t
+
+# Calculate Log Return
+def calculate_log_return(start_price, end_price):
+    return log(end_price / start_price)
+
+# Calculate Variance
+def calculate_variance(dataset):
+    mean = sum(dataset)/len(dataset)
+    numerator = 0
+    for data in dataset:
+        numerator += (data-mean) ** 2
+    return numerator / len(dataset)
+
+# Calculate Standard Deviation
+def calculate_stddev(dataset):
+    variance = calculate_variance(dataset)
+    return sqrt(variance)
+
+# Calculate Correlation Coefficient
+def calculate_correlation(set_x, set_y):
+    sum_x = sum(set_x)
+    sum_y = sum(set_y)
+    
+    sum_x2 = sum([x ** 2 for x in set_x])
+    sum_y2 = sum([y ** 2 for y in set_y])
+    sum_xy = sum([x * y for x,y in zip(set_x, set_y)])
+    
+    n = len(set_x)
+    numerator = n * sum_xy - sum_x * sum_y
+    denominator = sqrt((n * sum_x2 - sum_x ** 2) * (n * sum_y2 - sum_y ** 2))
+    return numerator / denominator
